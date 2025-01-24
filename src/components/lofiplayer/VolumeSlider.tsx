bb711404adede232f21f5b7f6a557b9a16429912
Kaung Set <kset2299@gmail.com>
2025-01-24 14:11:27 +0630

    add volume slider

---
+++ src/components/lofiplayer/VolumeSlider.tsx
@@ -0,0 +1,12 @@
+const VolumeSlider = () => {
+  return (
+    <div className="w-full h-1 bg-white relative col-span-4 rounded-lg">
+      <div className="absolute w-1/2 h-full top-0 bottom-0 left-0 bg-textPrimary rounded-l-lg"></div>
+      <div
+        className="absolute size-[10px] rounded-full bg-textPrimary top-1/2 transform -translate-y-1/2"
+        style={{ left: "49%" }}
+      />
+    </div>
+  );
+};
+export default VolumeSlider;
